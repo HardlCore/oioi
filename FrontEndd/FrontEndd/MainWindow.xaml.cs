c5fcc1fe39e1cc2bb92c19fcb65e81ee841da652
HardlCore <110555113+HardlCore@users.noreply.github.com>
2022-08-04 15:40:25 -0300

    Add files via upload

---
+++ FrontEndd/FrontEndd/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace FrontEndd
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        bool click = true;
+        int op = 0;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public object Telas { get; private set; }
+
+        private void Alterar(object sender, SelectionChangedEventArgs e)
+            
+
+
+        {
+
+            op = Esconder.SelectedIndex;
+            if (op >= 0) ;
+            switch (op)
+            {
+                case 0:
+                    Telas.Children.Clear();
+                    Telas.Children.Add(new Tela());
+
+                    break; 
+            }
+                
+
+                
+
+
+        }
+       
+
+
+
+
+          
+
+        
+    }
+       
+    }
+
---
+++ FrontEndd/FrontEndd/Tela.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FrontEndd
+{
+    /// <summary>
+    /// Interação lógica para Tela.xam
+    /// </summary>
+    public partial class Tela : UserControl
+    {
+        public Tela()
+        {
+            InitializeComponent();
+        }
+
+        public static object Children { get; internal set; }
+    }
+}
